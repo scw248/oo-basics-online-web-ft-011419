897277534f221a9cd99045cf5fdbb9cc59be9473
Stephen Wright <scw248@gmail.com>
2019-01-22 15:24:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,16 @@
 class Book
-  def initialize(title, author)
+  attr_accessor :author, :page_count, genre
+  
+  attr_reader :title
+  
+  def initialize(title)
     @title = title
   end
   
-  def title
-    @title
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
+    
   
 end
 
