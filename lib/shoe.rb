e1014bba432e8e4b99ee1e4e962cd9ee444a0c7a
Stephen Wright <scw248@gmail.com>
2019-01-22 15:27:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
-# Make your shoe class here!
+class Shoe
+  attr_reader :brand, :color, :size
+  
+  def initialize(brand, color, size)
+    @brand = brand
+    @color = color
+    @size = size
+  end
+  
+end  
+  
